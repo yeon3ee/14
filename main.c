86f0bc73a43e123ebbd8cc3b118ec331a039d62a
yeon3ee <2310538@sookmyung.ac.kr>
2023-12-08 14:48:50 +0900

    01

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void) {
+	char *pc = NULL;
+	int i = 0;
+	
+	pc = (char*)malloc(100*sizeof(char));
+	if (pc == NULL) {
+		printf("메모리 할당 error\n");
+		exit(1);
+}
+
+for (i=0;i<26;i++) {
+	pc[i] = 'a'+i;
+}
+pc[i] = 0;
+printf("%s\n", pc);
+free(pc);
+}
